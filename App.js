9b2086f3feff2a5e91061e6e5bf073c758b56317
doref26 <62805997+doref26@users.noreply.github.com>
2022-03-09 00:01:14 +0200

    09.03

--- App.js
+++ App.js
@@ -1,48 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
+const image = { uri: "http://proj.ruppin.ac.il/bgroup89/Fantasy_league_proj/%D7%9C%D7%95%D7%92%D7%95.PNG" };
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ImageBackground source={require('../Fantasy_League/Images/logo.PNG')}
-                       style={{ flex: 1 }}>
-        <Text style={styles.container}>ברוכים הבאים</Text>
-        <StatusBar style="auto" />
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <View style={styles.container}>
+        <Text style={styles.container}></Text>
         <View style={styles.Button}>
-          <Button title='new user' ButtonStyle={{ borderColor: 'black', border: 'solid black', borderWidth: '3px' }}></Button>
-          <Button title='existing user' ButtonStyle={styles.ButtonStyle}></Button>
+          <Button title='new user' onPress={''}></Button>
+          <Text>                                  </Text>
+          <Button title='existing user' onPress={''}></Button>
         </View>
-
-      </ImageBackground >
-    </View >
-  ); 
+      </View >
+      <StatusBar style="auto" />
+    </ImageBackground>
+  );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    ImageBackground: '../Fantasy_League/Images/logo.PNG',
-    backgroundColor: 'null',
     alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: '50px',
+    //justifyContent: 'space-around',
+    fontSize: 50,
     fontWeight: 'bold',
-    marginLeft: 100,
-    marginRight: 100
+    marginTop: 30,
   },
   Button: {
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginHorizontal: 20,
-    marginTop: 5,
-    justifyContent: 'space-around',
-    marginBottom: '50px'
-
+    marginTop: 0,
+    marginBottom: 50,
   },
-  ButtonStyle: {
-    border: 'solid black',
-    borderWidth: '3px',
-    color: "null",
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    resizeMode: 'cover'
   },
 });
--- Components/LoginPage.jsx
+++ Components/LoginPage.jsx
@@ -1,9 +1,24 @@
-import {} from 'react-native'
+import { Text, View, } from 'react-native'
 
 const LoginPage = () => {
     return (
-        <h1></h1>
-        );
-}   
+        <View>
+            <TextInput
+                onChangeText={(val) => this.setState({ username: val })}
+                //keyboardType='email-address'
+                //placeholder='Email or Mobile Num'
+                //placeholderTextColor='rgba(225,225,225,0.7)'
+                //underlineColorAndroid='transparent' 
+                />
+            <TextInput
+                onChangeText={(val) => this.setState({ password: val })}
+                //placeholder='Password'
+                //placeholderTextColor='rgba(225,225,225,0.7)'
+                //underlineColorAndroid='transparent'
+                //secureTextEntry 
+                />
+        </View>
+    );
+}
 
 export default LoginPage;
